8e051111a3eb95dbcd9e99afc4c8e949d517b62a
Rasmus Jensen <rasmusdj96@gmail.com>
2022-08-19 10:16:30 +0200

    Built loop - declared some elements

--- js/loops.js
+++ js/loops.js
@@ -1,6 +1,5 @@
 // data to have fun with
-const myTestarray = ['array tiger', 'array elefant', 'array edderkop', 'array koala'];
-const myTestObject = {
+const myAnimalObject = {
     elephant: { name: 'elefant', picture: '/img/elephant.jpg', description: 'Elephants are the largest existing land animals. Three living species are currently recognised: the African bush elephant, the African forest elephant, and the Asian elephant. They are the only surviving members of the family Elephantidae and the order Proboscidea. The order was formerly much more diverse during the Pleistocene, but most species became extinct during the Late Pleistocene epoch. Distinctive features of elephants include a long proboscis called a trunk, tusks, large ear flaps, pillar-like legs, and tough but sensitive skin. The trunk is used for breathing, bringing food and water to the mouth, and grasping objects. Tusks, which are derived from the incisor teeth, serve both as weapons and as tools for moving objects and digging. The large ear flaps assist in maintaining a constant body temperature as well as in communication. African elephants have larger ears and concave backs, whereas Asian elephants have smaller ears, and convex or level backs.' },
     tiger: { name: 'tiger', picture: '/img/standard_tiger.jpg', description: 'en flot tiger' },
     spider: { name: 'edderkop', picture: '/img/Brachypelma_smithi.jpg', description: 'Mexican red knee tarantula spider Brahipelma Smitti on coconut substrate shortly after molting' },
@@ -9,60 +8,33 @@ const myTestObject = {
 };
 
 
-// the app lives here
+// Declare and create elements
 const myApp = document.getElementById('app');
+const cardFigure = document.createElement("figure")
+const cardHeadline = document.createElement("h2")
 
 
 
 
+//Loop
+for (let myIndex in myAnimalObject){
+    // console.log(myAnimalObject[myIndex].name);
+    // console.log(myAnimalObject[myIndex].picture);
+    // console.log(myAnimalObject[myIndex].description);
 
-// for loop
-for (let i = 0; i < 5; i++) {
-    console.log('step ' + i);
+    buildCard(myAnimalObject[myIndex].name, myAnimalObject[myIndex].picture, myAnimalObject[myIndex].description)
+
+}
+
+testCard()
+
+
+
+//Card Function
+
+function buildCard(myHeadline, img, myDescription){
+
 }
 
 
-
-// while loop remember to make the expression get to false otherwise the browser crashes.
-let i = 0;
-while (i < 10) {
-    console.log("The number is " + i);
-    i++;
-}
-
-
-/* // for in loop used with arrays
-for (let myIndex in myTestarray) {
-    console.log(myTestarray[myIndex]);
-    testCard(myTestarray[myIndex], 'app')
-}
-
-
-// for loop to use with objects and keys
-
-for (let myIndex in myTestObject) {
-    console.log(myTestObject[myIndex].name);
-    testCard(myTestObject[myIndex].name, 'app')
-} */
-
-
-
-
-
-// debug functions ----------------------------------------------------------------------------
-//  creates a simple headline with the textmsg in a h2 in the supplied id
-// testCard('weee', 'app');
-function testCard(textMsg, myId, clearContainer) {
-    let myParent = document.getElementById(myId);
-
-    // clear if clearContainer is set
-    if (clearContainer) {
-        myParent.innerHTML = '';
-    }
-
-    // create elements
-    let myHeadline = document.createElement('h2');
-    myHeadline.innerText = textMsg;
-    myParent.appendChild(myHeadline);
-
-}
+//Appends
